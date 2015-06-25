f26b100504dafe5090906a3b082b26970eb4a687
codeamac02 <codeamac02@gmail.com>
2015-06-25 14:27:39 -0500

    Add ajax so the die can roll without refreshing the entire page

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,19 +1,21 @@
 get '/' do
 	@roll = Roll.create
+	# @dice = params[:value]
   erb :index
 end
 
 
 # QUE HACER?: Convierte esta ruta para que utilice Ajax y si la petición no es de Ajax de igual forma sirva. 
 post '/rolls' do
+	puts"entre a rolls"
 	# Si el user nos pasa un valor, lo usamos y so no creamos uno. 
-
   value = params[:value] ? params[:value].to_i : nil
-
-  @roll = value ? Roll.create({ value: value }) : Roll.create
-
+	# puts "Esto es value: #{value}"
+	p value
+  @roll = value ? Roll.create(value: value) : Roll.create
+	puts "Esto es @roll: #{@roll.value}"
   @roll = params[:value] ? Roll.create({ value: params[:value].to_i }) : Roll.create
-
-  erb :index  # TIP: Que esta haciendo esto y que debe hacer diferente. 
-
+	puts "Esto es after @roll: #{@roll.value}"
+  # erb :index  # TIP: Que esta haciendo esto y que debe hacer diferente. 
+  erb :_die_roll, layout: false
 end
--- public/js/application.js
+++ public/js/application.js
@@ -3,5 +3,15 @@ $(document).ready(function() {
 	// completamente. 
 	// Esto garantiza que si amarramos(bind) una función a un elemento 
 	// de HTML este exista ya en la página.
-  	
+
+	$("form").on("submit", function( event ) {
+		event.preventDefault();
+		console.log("Ya tengo control");
+
+		var url = $("form").attr("action");
+
+		var post = $.post(url, function(data) {
+			$("#die").html(data);
+		});
+	});
 });
